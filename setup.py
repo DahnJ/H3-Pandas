a3b5951afd5a24c54be0f0f883a0048cff121c28
dahn <daniel.jahn@targomo.com>
2021-05-22 12:24:31 +0200

    DOC: long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="h3pandas",
     version=0.1,
     license="MIT",
     description="Integration of H3 and GeoPandas",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Dahn",
     author_email="dahnjahn@gmail.com",
     url="https://github.com/DahnJ/H3-Pandas",
