03c380c6e328c1a27a0f8d88a8a35e80ba9dc4cb
dahn <dahnjahn@gmail.com>
2021-05-31 21:20:40 +0200

    BLD: build-conda uses grayskull and noarch build
    grayskull seems to work better than conda skeleton
    noarch build seems to be all that's needed
    (namely there is no need to convert to all platforms manually)

--- build-conda.sh
+++ build-conda.sh
@@ -3,38 +3,26 @@
 
 # Variables
 pkg='h3pandas'
-# array=( 3.7 3.8 3.9 ) TODO: 3.9 does not work because of dependency conflicts
-array=( 3.6 3.7 3.8 )
+array=( 3.7 3.8 3.9 ) 
+# array=( 3.6 3.7 3.8 3.9 )
 
 # Constants
 MINICONDA=$HOME/miniconda3
 
-echo "Building conda package ..."
-cd ~
-conda skeleton pypi $pkg
+#echo "Building conda package ..."
+cd $HOME
+grayskull pypi $pkg
 
 # update meta.yaml
 echo "Updating meta.yaml ..."
 sed -i 's/^\(\s\+- h3\)$/\1-py/g' $pkg/meta.yaml
-sed -i 's/^\(\s\+-\) your-github-id-here/\1 18722560/g' $pkg/meta.yaml
+sed -i 's/^\(\s\+-\) your-github-id-here/\1 DahnJ/g' $pkg/meta.yaml
 
 # building conda packages
 for i in "${array[@]}"
 do
     echo "Building for Python $i"
 	conda-build --python $i $pkg
-done
-
-# convert package to other platforms
-cd ~
-platforms=( osx-64 linux-32 linux-64 win-32 win-64 )
-find $MINICONDA/conda-bld/linux-64/ -name *.tar.bz2 | while read file
-do
-    echo $file
-    for platform in "${platforms[@]}"
-    do
-       conda convert --platform $platform $file  -o $MINICONDA/conda-bld/
-    done    
 done
 
 # upload packages to conda
@@ -46,4 +34,4 @@ done
 echo "Building conda package done!"
 
 rm $HOME/h3pandas -r
-conda build purge
+conda build purge-all
