3738b63b9f710fe7bf49aae20f91fec2910ed9c5
dahn <dahnjahn@gmail.com>
2021-06-01 23:00:44 +0200

    DOC: Attempt to fix readthedocs.io
    It appears that readthedocs fails because Literal from typing
    is only for Python >=3.8. This commit attempts to fix that.

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -1,4 +1,4 @@
 # /bin/bash
 make clean
 make html # behaves weirdly the first time, investigate later
-make html
+# make html
--- docs/source/modules.rst
+++
@@ -1,8 +0,0 @@
-h3pandas
-========
-
-.. toctree::
-   :maxdepth: 4
-
-
-* :ref:`oasijdfij<h3pandas\.h3pandas module>`
