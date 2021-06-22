ba6511a72c98501d3c839815fbb7695b6da4ff9f
dahn <dahnjahn@gmail.com>
2021-06-22 21:18:08 +0200

    DOC: Add Examples, See Also, minor fixes

--- h3pandas/h3pandas.py
+++ h3pandas/h3pandas.py
@@ -61,6 +61,28 @@ class H3Accessor:
         See Also
         --------
         geo_to_h3_aggregate : Extended API method that aggregates points by H3 address
+        
+        Examples
+        --------
+        >>> df = pd.DataFrame({'lat': [50, 51], 'lng':[14, 15]})
+        >>> df.h3.geo_to_h3(8)
+                         lat  lng
+        h3_08
+        881e309739fffff   50   14
+        881e2659c3fffff   51   15
+
+        >>> df.h3.geo_to_h3(8, set_index=False)
+           lat  lng            h3_08
+        0   50   14  881e309739fffff
+        1   51   15  881e2659c3fffff
+
+        >>> gdf = gpd.GeoDataFrame({'val': [5, 1]}, geometry=gpd.points_from_xy(x=[14, 15], y=(50, 51)))
+        >>> gdf.h3.geo_to_h3(8)
+                         val                   geometry
+        h3_08
+        881e309739fffff    5  POINT (14.00000 50.00000)
+        881e2659c3fffff    1  POINT (15.00000 51.00000)
+
         """
         if isinstance(self._df, gpd.GeoDataFrame):
             lngs = self._df.geometry.x
@@ -96,6 +118,15 @@ class H3Accessor:
         See Also
         --------
         h3_to_geo_boundary : Adds a hexagonal cell
+        
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', '881e2659c3fffff'])
+        >>> df.h3.h3_to_geo()
+                         val                   geometry
+        881e309739fffff    5  POINT (14.00037 50.00055)
+        881e2659c3fffff    1  POINT (14.99715 51.00252)
+
         """
         return self._apply_index_assign(
             h3.h3_to_geo,
@@ -115,6 +146,14 @@ class H3Accessor:
         ------
         ValueError
             When an invalid H3 address is encountered
+
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', '881e2659c3fffff'])
+        >>> df.h3.h3_to_geo_boundary()
+                         val                                           geometry
+        881e309739fffff    5  POLYGON ((13.99527 50.00368, 13.99310 49.99929...
+        881e2659c3fffff    1  POLYGON ((14.99201 51.00565, 14.98973 51.00133...
         """
         return self._apply_index_assign(
             wrapped_partial(h3.h3_to_geo_boundary, geo_json=True),
@@ -125,14 +164,41 @@ class H3Accessor:
 
     @doc_standard("h3_resolution", "containing the resolution of each H3 address")
     def h3_get_resolution(self) -> AnyDataFrame:
+        """
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', '881e2659c3fffff'])
+        >>> df.h3.h3_get_resolution()
+                         val  h3_resolution
+        881e309739fffff    5              8
+        881e2659c3fffff    1              8
+        """
         return self._apply_index_assign(h3.h3_get_resolution, "h3_resolution")
 
     @doc_standard("h3_base_cell", "containing the base cell of each H3 address")
     def h3_get_base_cell(self):
+        """
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', '881e2659c3fffff'])
+        >>> df.h3.h3_get_base_cell()
+                         val  h3_base_cell
+        881e309739fffff    5            15
+        881e2659c3fffff    1            15
+        """
         return self._apply_index_assign(h3.h3_get_base_cell, "h3_base_cell")
 
     @doc_standard("h3_is_valid", "containing the validity of each H3 address")
     def h3_is_valid(self):
+        """
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', 'INVALID'])
+        >>> df.h3.h3_is_valid()
+                         val  h3_is_valid
+        881e309739fffff    5         True
+        INVALID            1        False
+        """
         return self._apply_index_assign(h3.h3_is_valid, "h3_is_valid")
 
     @doc_standard(
@@ -153,6 +219,31 @@ class H3Accessor:
         --------
         k_ring_smoothing : Extended API method that distributes numeric values
             to the k-ring cells
+
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', '881e2659c3fffff'])
+        >>> df.h3.k_ring(1)
+                         val                                          h3_k_ring
+        881e309739fffff    5  [881e30973dfffff, 881e309703fffff, 881e309707f...
+        881e2659c3fffff    1  [881e2659ddfffff, 881e2659c3fffff, 881e2659cbf...
+
+        >>> df.h3.k_ring(1, explode=True)
+                         val        h3_k_ring
+        881e2659c3fffff    1  881e2659ddfffff
+        881e2659c3fffff    1  881e2659c3fffff
+        881e2659c3fffff    1  881e2659cbfffff
+        881e2659c3fffff    1  881e2659d5fffff
+        881e2659c3fffff    1  881e2659c7fffff
+        881e2659c3fffff    1  881e265989fffff
+        881e2659c3fffff    1  881e2659c1fffff
+        881e309739fffff    5  881e30973dfffff
+        881e309739fffff    5  881e309703fffff
+        881e309739fffff    5  881e309707fffff
+        881e309739fffff    5  881e30973bfffff
+        881e309739fffff    5  881e309715fffff
+        881e309739fffff    5  881e309739fffff
+        881e309739fffff    5  881e309731fffff
         """
         func = wrapped_partial(h3.k_ring, k=k)
         column_name = "h3_k_ring"
@@ -161,7 +252,7 @@ class H3Accessor:
         return self._apply_index_assign(func, column_name, list)
 
     @doc_standard(
-        "h3_k_ring",
+        "h3_hex_ring",
         "containing a list H3 addresses forming a hollow hexagonal ring"
         "at a distance `k`",
     )
@@ -175,6 +266,28 @@ class H3Accessor:
             If True, will explode the resulting list vertically.
             All other columns' values are copied.
             Default: False
+
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', '881e2659c3fffff'])
+        >>> df.h3.hex_ring(1)
+                         val                                        h3_hex_ring
+        881e309739fffff    5  [881e30973dfffff, 881e309703fffff, 881e309707f...
+        881e2659c3fffff    1  [881e2659ddfffff, 881e2659cbfffff, 881e2659d5f...
+        >>> df.h3.hex_ring(1, explode=True)
+                         val      h3_hex_ring
+        881e2659c3fffff    1  881e2659ddfffff
+        881e2659c3fffff    1  881e2659cbfffff
+        881e2659c3fffff    1  881e2659d5fffff
+        881e2659c3fffff    1  881e2659c7fffff
+        881e2659c3fffff    1  881e265989fffff
+        881e2659c3fffff    1  881e2659c1fffff
+        881e309739fffff    5  881e30973dfffff
+        881e309739fffff    5  881e309703fffff
+        881e309739fffff    5  881e309707fffff
+        881e309739fffff    5  881e30973bfffff
+        881e309739fffff    5  881e309715fffff
+        881e309739fffff    5  881e309731fffff
         """
         func = wrapped_partial(h3.hex_ring, k=k)
         column_name = "h3_hex_ring"
@@ -194,6 +307,14 @@ class H3Accessor:
         --------
         h3_to_parent_aggregate : Extended API method that aggregates cells by their
             parent cell
+            
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', '881e2659c3fffff'])
+        >>> df.h3.h3_to_parent(5)
+                         val            h3_05
+        881e309739fffff    5  851e3097fffffff
+        881e2659c3fffff    1  851e265bfffffff
         """
         # TODO: Test `h3_parent` case
         column = self._format_resolution(resolution) if resolution else "h3_parent"
@@ -209,6 +330,14 @@ class H3Accessor:
         resolution : int or None
             H3 resolution. If none, then returns the child of resolution
             directly below that of each H3 cell
+            
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', '881e2659c3fffff'])
+        >>> df.h3.h3_to_center_child()
+                         val  h3_center_child
+        881e309739fffff    5  891e3097383ffff
+        881e2659c3fffff    1  891e2659c23ffff
         """
         return self._apply_index_assign(
             wrapped_partial(h3.h3_to_center_child, res=resolution), "h3_center_child"
@@ -233,6 +362,24 @@ class H3Accessor:
         --------
         polyfill_resample : Extended API method that distributes the polygon's values
             to the H3 cells contained in it
+            
+        Examples
+        --------
+        >>> from shapely.geometry import box
+        >>> gdf = gpd.GeoDataFrame(geometry=[box(0, 0, 1, 1)])
+        >>> gdf.h3.polyfill(4)
+                                                    geometry                                        h3_polyfill
+        0  POLYGON ((1.00000 0.00000, 1.00000 1.00000, 0....  [84754e3ffffffff, 84754c7ffffffff, 84754c5ffff...
+        >>> gdf.h3.polyfill(4, explode=True)
+                                                    geometry      h3_polyfill
+        0  POLYGON ((1.00000 0.00000, 1.00000 1.00000, 0....  84754e3ffffffff
+        0  POLYGON ((1.00000 0.00000, 1.00000 1.00000, 0....  84754c7ffffffff
+        0  POLYGON ((1.00000 0.00000, 1.00000 1.00000, 0....  84754c5ffffffff
+        0  POLYGON ((1.00000 0.00000, 1.00000 1.00000, 0....  84754ebffffffff
+        0  POLYGON ((1.00000 0.00000, 1.00000 1.00000, 0....  84754edffffffff
+        0  POLYGON ((1.00000 0.00000, 1.00000 1.00000, 0....  84754e1ffffffff
+        0  POLYGON ((1.00000 0.00000, 1.00000 1.00000, 0....  84754e9ffffffff
+        0  POLYGON ((1.00000 0.00000, 1.00000 1.00000, 0....  8475413ffffffff
         """
 
         def func(row):
@@ -260,6 +407,14 @@ class H3Accessor:
         ----------
         unit : str, options: 'km^2', 'm^2', or 'rads^2'
             Unit for area result. Default: 'km^2`
+            
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', '881e2659c3fffff'])
+        >>> df.h3.cell_area()
+                         val  h3_cell_area
+        881e309739fffff    5      0.695651
+        881e2659c3fffff    1      0.684242
         """
         return self._apply_index_assign(
             wrapped_partial(h3.cell_area, unit=unit), "h3_cell_area"
@@ -296,10 +451,35 @@ class H3Accessor:
             (Optional) Whether to add a `geometry` column with the hexagonal cells.
             Default = True
 
-
         Returns
         -------
         (Geo)DataFrame aggregated by H3 address into which each row's point falls
+
+        See Also
+        --------
+        geo_to_h3 : H3 API method upon which this function builds
+        
+        Examples
+        --------
+        >>> df = pd.DataFrame({'lat': [50, 51], 'lng':[14, 15], 'val': [10, 1]})
+        >>> df.h3.geo_to_h3(1)
+                         lat  lng  val
+        h3_01
+        811e3ffffffffff   50   14   10
+        811e3ffffffffff   51   15    1
+        >>> df.h3.geo_to_h3_aggregate(1)
+                         val                                           geometry
+        h3_01
+        811e3ffffffffff   11  POLYGON ((12.34575 50.55428, 12.67732 46.40696...
+        >>> df = pd.DataFrame({'lat': [50, 51], 'lng':[14, 15], 'val': [10, 1]})
+        >>> df.h3.geo_to_h3_aggregate(1, operation='mean')
+                         val                                           geometry
+        h3_01
+        811e3ffffffffff  5.5  POLYGON ((12.34575 50.55428, 12.67732 46.40696...
+        >>> df.h3.geo_to_h3_aggregate(1, return_geometry=False)
+                         val
+        h3_01
+        811e3ffffffffff   11
         """
         grouped = pd.DataFrame(
             self.geo_to_h3(resolution, lat_col, lng_col, False)
@@ -336,6 +516,30 @@ class H3Accessor:
         ------
         ValueError
             When an invalid H3 address is encountered
+
+        See Also
+        --------
+        h3_to_parent : H3 API method upon which this function builds
+
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', '881e2659c3fffff'])
+        >>> df.h3.h3_to_parent(1)
+                         val            h3_01
+        881e309739fffff    5  811e3ffffffffff
+        881e2659c3fffff    1  811e3ffffffffff
+        >>> df.h3.h3_to_parent_aggregate(1)
+                         val                                           geometry
+        h3_01
+        811e3ffffffffff    6  POLYGON ((12.34575 50.55428, 12.67732 46.40696...
+        >>> df.h3.h3_to_parent_aggregate(1, operation='mean')
+                         val                                           geometry
+        h3_01
+        811e3ffffffffff    3  POLYGON ((12.34575 50.55428, 12.67732 46.40696...
+        >>> df.h3.h3_to_parent_aggregate(1, return_geometry=False)
+                         val
+        h3_01
+        811e3ffffffffff    6
         """
         parent_h3addresses = [
             catch_invalid_h3_address(h3.h3_to_parent)(h3address, resolution)
@@ -385,6 +589,64 @@ class H3Accessor:
         -------
         (Geo)DataFrame with smoothed values
 
+        See Also
+        --------
+        k_ring : H3 API method upon which this method builds
+
+        Examples
+        --------
+        >>> df = pd.DataFrame({'val': [5, 1]}, index=['881e309739fffff', '881e2659c3fffff'])
+        >>> df.h3.k_ring_smoothing(1)
+                              val                                           geometry
+        h3_k_ring
+        881e265989fffff  0.142857  POLYGON ((14.99488 50.99821, 14.99260 50.99389...
+        881e2659c1fffff  0.142857  POLYGON ((14.97944 51.00758, 14.97717 51.00326...
+        881e2659c3fffff  0.142857  POLYGON ((14.99201 51.00565, 14.98973 51.00133...
+        881e2659c7fffff  0.142857  POLYGON ((14.98231 51.00014, 14.98004 50.99582...
+        881e2659cbfffff  0.142857  POLYGON ((14.98914 51.01308, 14.98687 51.00877...
+        881e2659d5fffff  0.142857  POLYGON ((15.00458 51.00371, 15.00230 50.99940...
+        881e2659ddfffff  0.142857  POLYGON ((15.00171 51.01115, 14.99943 51.00684...
+        881e309703fffff  0.714286  POLYGON ((13.99235 50.01119, 13.99017 50.00681...
+        881e309707fffff  0.714286  POLYGON ((13.98290 50.00555, 13.98072 50.00116...
+        881e309715fffff  0.714286  POLYGON ((14.00473 50.00932, 14.00255 50.00494...
+        881e309731fffff  0.714286  POLYGON ((13.99819 49.99617, 13.99602 49.99178...
+        881e309739fffff  0.714286  POLYGON ((13.99527 50.00368, 13.99310 49.99929...
+        881e30973bfffff  0.714286  POLYGON ((14.00765 50.00181, 14.00547 49.99742...
+        881e30973dfffff  0.714286  POLYGON ((13.98582 49.99803, 13.98364 49.99365...
+        >>> df.h3.k_ring_smoothing(weights=[2, 1])
+                           val                                           geometry
+        h3_hex_ring
+        881e265989fffff  0.125  POLYGON ((14.99488 50.99821, 14.99260 50.99389...
+        881e2659c1fffff  0.125  POLYGON ((14.97944 51.00758, 14.97717 51.00326...
+        881e2659c3fffff  0.250  POLYGON ((14.99201 51.00565, 14.98973 51.00133...
+        881e2659c7fffff  0.125  POLYGON ((14.98231 51.00014, 14.98004 50.99582...
+        881e2659cbfffff  0.125  POLYGON ((14.98914 51.01308, 14.98687 51.00877...
+        881e2659d5fffff  0.125  POLYGON ((15.00458 51.00371, 15.00230 50.99940...
+        881e2659ddfffff  0.125  POLYGON ((15.00171 51.01115, 14.99943 51.00684...
+        881e309703fffff  0.625  POLYGON ((13.99235 50.01119, 13.99017 50.00681...
+        881e309707fffff  0.625  POLYGON ((13.98290 50.00555, 13.98072 50.00116...
+        881e309715fffff  0.625  POLYGON ((14.00473 50.00932, 14.00255 50.00494...
+        881e309731fffff  0.625  POLYGON ((13.99819 49.99617, 13.99602 49.99178...
+        881e309739fffff  1.250  POLYGON ((13.99527 50.00368, 13.99310 49.99929...
+        881e30973bfffff  0.625  POLYGON ((14.00765 50.00181, 14.00547 49.99742...
+        881e30973dfffff  0.625  POLYGON ((13.98582 49.99803, 13.98364 49.99365...
+        >>> df.h3.k_ring_smoothing(1, return_geometry=False)
+                              val
+        h3_k_ring
+        881e265989fffff  0.142857
+        881e2659c1fffff  0.142857
+        881e2659c3fffff  0.142857
+        881e2659c7fffff  0.142857
+        881e2659cbfffff  0.142857
+        881e2659d5fffff  0.142857
+        881e2659ddfffff  0.142857
+        881e309703fffff  0.714286
+        881e309707fffff  0.714286
+        881e309715fffff  0.714286
+        881e309731fffff  0.714286
+        881e309739fffff  0.714286
+        881e30973bfffff  0.714286
+        881e30973dfffff  0.714286
         """
         if sum([weights is None, k is None]) != 1:
             raise ValueError("Exactly one of `k` and `weights` must be set.")
@@ -433,12 +695,13 @@ class H3Accessor:
         return result.h3.h3_to_geo_boundary() if return_geometry else result
 
     # TODO: Test
-    # TODO: Provide a warning if sums don't agree or sth like that?
+    # TODO: Provide a warning if sums don't agree or something like that?
     #  (For uncovered polygons)
     def polyfill_resample(
         self, resolution: int, return_geometry: bool = True
     ) -> AnyDataFrame:
-        """Experimental
+        """Experimental. Currently essentially polyfill(..., explode=True) that
+        sets the H3 index and adds the H3 cell geometry.
 
         Parameters
         ----------
@@ -450,7 +713,27 @@ class H3Accessor:
 
         Returns
         -------
+        (Geo)DataFrame with H3 cells with centroids within the input polygons.
 
+        See Also
+        --------
+        polyfill : H3 API method upon which this method builds
+
+        Examples
+        --------
+        >>> from shapely.geometry import box
+        >>> gdf = gpd.GeoDataFrame(geometry=[box(0, 0, 1, 1)])
+        >>> gdf.h3.polyfill_resample(4)
+                         index                                           geometry
+        h3_polyfill
+        84754e3ffffffff      0  POLYGON ((0.33404 -0.11975, 0.42911 0.07901, 0...
+        84754c7ffffffff      0  POLYGON ((0.92140 -0.03115, 1.01693 0.16862, 0...
+        84754c5ffffffff      0  POLYGON ((0.91569 0.33807, 1.01106 0.53747, 0....
+        84754ebffffffff      0  POLYGON ((0.62438 0.10878, 0.71960 0.30787, 0....
+        84754edffffffff      0  POLYGON ((0.32478 0.61394, 0.41951 0.81195, 0....
+        84754e1ffffffff      0  POLYGON ((0.32940 0.24775, 0.42430 0.44615, 0....
+        84754e9ffffffff      0  POLYGON ((0.61922 0.47649, 0.71427 0.67520, 0....
+        8475413ffffffff      0  POLYGON ((0.91001 0.70597, 1.00521 0.90497, 0....
         """
         result = self._df.h3.polyfill(resolution, explode=True)
         result = result.reset_index().set_index(COLUMN_H3_POLYFILL)
